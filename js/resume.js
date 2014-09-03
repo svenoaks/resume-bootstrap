9f324d5a554d33b8e88e731ea36f6947b657c260
svenoaks <svenoaks@gmail.com>
2014-09-02 21:45:32 -0400

    navbar

--- js/resume.js
+++ js/resume.js
@@ -3,19 +3,51 @@
  */
 var COLOR_ICON_GRAY = "#777777";
 var COLOR_LINK = "#428bca";
-var TIME_MS_ICON_ANIMATE = "176";
+
+var TIME_MS_ICON_ANIMATE = "50";
 var TIME_MS_FADEIN_BODY = 500;
 
-$(".no-link").hover(
-    function() {
-        $(this)
-            .css('color',COLOR_ICON_GRAY)
-            .animate({'color': COLOR_LINK}, TIME_MS_ICON_ANIMATE);
-    },
-    function() {
-        $(this)
-            .animate({'color': COLOR_ICON_GRAY}, TIME_MS_ICON_ANIMATE);
-    }
-);
+$( document ).ready(function() {
 
-$(".js .container").fadeIn(TIME_MS_FADEIN_BODY);
+    $(".js .container").fadeIn(TIME_MS_FADEIN_BODY);
+
+    $(".no-link").hover(
+        function() {
+            $(this)
+                .clearQueue()
+                .css('color',COLOR_LINK)
+                .animate({'color': COLOR_ICON_GRAY}, TIME_MS_ICON_ANIMATE);
+        },
+        function() {
+            $(this)
+                .animate({'color': COLOR_LINK}, TIME_MS_ICON_ANIMATE);
+        }
+    );
+    $(window).scroll(function() {
+        var pos = $(window).scrollTop();
+        var cur_width = $(".container").css('width');
+
+        if (pos > 55 && cur_width == '970px') {
+            $(".row-nav").css({
+                "position": "fixed",
+                "top":    "0",
+                "width":    "970px",
+                "margin-bottom":    "55px",
+            });
+            $("#aftermenuspace").css({
+                "height":    "55px"
+            });
+        } else {
+            $(".row-nav").css({
+                "position": "relative",
+                "margin-bottom":    "0",
+                "width":    "initial"
+            });
+            $("#aftermenuspace").css({
+                "height":    "0"
+            });
+        }
+    });
+});
+
+
