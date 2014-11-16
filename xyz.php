6ad63d126836f430b6b4e60beed87103b9e2755f
svenoaks <svenoaks@gmail.com>
2014-11-16 14:30:27 -0500

    template

--- xyz.php
+++ xyz.php
@@ -15,6 +15,9 @@ $text = $name . $newLine . $email . $newLine . $message;
 
 $text = str_replace("\n.", "\n..", $text);
 
-$success = mail ( "svenoaks@gmail.com" , "Email from Resume" , $text);
+/*The email from your resume site will come from whatever address you set here.
+We place the email address of the sender in the body to prevent spam.*/
+
+$success = mail ( "myemailaddress@host.com" , "Email from Resume" , $text);
 
 if ($success) exit('success'); else exit('failure');
